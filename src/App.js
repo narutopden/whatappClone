272ab761747a771e009be04318e5ca168cd98eda
narutopden <narutop49@gmail.com>
2021-01-23 17:11:33 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      this is whats app clone
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAd87wogkcOLS-xqhTPUQIo64rAZ4OQyes",
+    authDomain: "whatsapp-clone-9ecf7.firebaseapp.com",
+    projectId: "whatsapp-clone-9ecf7",
+    storageBucket: "whatsapp-clone-9ecf7.appspot.com",
+    messagingSenderId: "678529498479",
+    appId: "1:678529498479:web:fae503250776551f9dfc9a",
+    measurementId: "G-SKTP7TW1V3"
+  };
